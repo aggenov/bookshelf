e9f41a707153a993a7fd81205c2bf79bbfba23f6
RenTensei <scs.bon7@gmail.com>
2023-05-06 20:36:51 +0300

    added: hover implementation, data-attributes

--- src/JS/BestSellers/MarkupGenerators/Book.js
+++ src/JS/BestSellers/MarkupGenerators/Book.js
@@ -1,7 +1,9 @@
 export function bookMarkup({ _id, book_image, title, author }) {
   return `
-    <div class="book-item" data-id="${_id}">
-      <img class="book-image-container" src="${book_image}" />
+    <div class="book-item" data-modal-id="${_id}">
+      <div class="book-image-wrapper">
+        <img class="book-image-container" src="${book_image}" />
+      </div>
       <h4 class="book-item-title">${title}</h4>
       <p class="book-item-author">${author}</p>
     </div>
