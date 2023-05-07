92f918e7216a6745943a0e2d1fd8e67ca4f7d19b
MAX999 <max999b@gmail.com>
2023-05-07 19:41:03 +0300

    localStoreg
    додана пагінація

--- src/JS/localStorage/pagination.js
+++ src/JS/localStorage/pagination.js
@@ -1,16 +1,28 @@
 import { saveStorage, loadStorage, removeStorage } from './localStorageAdd';
 
-// Функуія визначення кількості сторінок і виведення результатів пагінацією
-
-const paginationBooks = (booksInPage, ex) => {
+// Функція перевірки і виведення масиву для обробки
+const booksArrayOld = () => {
   const booksArrayOld = loadStorage('books');
   if (booksArrayOld === null) {
     console.log('there are no books in local storage');
   } else {
-    const pageCount = Math.ceil(booksArrayOld.length / booksInPage);
-    const startIndex = (ex - 1) * booksInPage;
-    const endIndex = ex * booksInPage;
-    const exPagination = booksArrayOld.slice(startIndex, endIndex);
-    return exPagination;
+    return booksArrayOld;
   }
+};
+// Функція визначення кількості сторінок
+const amountOfPg = booksInPage => {
+  const booksArrayOld = booksArrayOld();
+  const pageCount = Math.ceil(booksArrayOld.length / booksInPage);
+  return pageCount;
+};
+// Функуія визначеннявиведення результатів пагінацією
+const paginationBooks = (booksInPage, ex) => {
+  const booksArrayOld = booksArrayOld();
+  const startIndex = (ex - 1) * booksInPage;
+  let endIndex = ex * booksInPage;
+  if (booksArrayOld.length <= endIndex) {
+    endIndex = booksArrayOld.length - 1;
+  }
+  const exPagination = booksArrayOld.slice(startIndex, endIndex);
+  return exPagination;
 };
