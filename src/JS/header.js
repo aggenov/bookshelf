7aade691e492bf53d46966ee8fda15b0f286ac42
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-05-07 21:59:08 +0200

    up

--- src/JS/header.js
+++ src/JS/header.js
@@ -4,9 +4,6 @@ const burgerMenu = document.querySelector('.modal-mobile-menu');
 const btnOpen = document.querySelector('.header-burger');
 const closeModalMenu = document.querySelector('.mobile-close-btn');
 
-console.log(burgerMenu);
-console.log(btnOpen);
-console.log(closeModalMenu);
 
 btnOpen.addEventListener("click", () => {
     burgerMenu.style.display = "block";
