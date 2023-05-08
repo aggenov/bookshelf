799f249ea221b17a091357b16a8b27ad1885c1ce
RenTensei <scs.bon7@gmail.com>
2023-05-08 14:02:56 +0300

    fix async issues and styles

--- src/JS/categories.js
+++ src/JS/categories.js
@@ -4,34 +4,47 @@ import { BASE_URL } from './home-by-category';
 
 const categoryList = document.querySelector('.categories-list');
 
-getData('/books/category-list').then(console.log);
+// привет от андрея :)
+getData('/books/category-list').then(data => {
+  console.log(data);
+  const markup = data.map(CategoryItem).join('');
 
-async function GetCategoryList() {
-  const resp = await axios.get(`${BASE_URL}category-list`);
-  console.log(resp.data);
-  return resp.data;
+  // console.log(markup);
+  categoryList.insertAdjacentHTML('beforeend', markup);
+});
+
+function CategoryItem({ list_name }) {
+  return `
+    <li class="category-name" data-category=${list_name}>${list_name}</li>
+  `;
 }
 
-function renderCategories(data) {
-  return data
-    .map(
-      data =>
-        `<li>
-            <button type="button" class="category-item" data-category="${data.list_name}>${data.list_name}</button>
-        </li>`
-    )
-    .join('');
-}
+// все что ниже нам больше не нужно :)
 
-async function titlesCategories() {
-  try {
-    const data = await GetCategoryList();
-    const markup = renderCategories(data);
-    // console.log(markup);
+// async function GetCategoryList() {
+//   const resp = await axios.get(`${BASE_URL}category-list`);
+//   console.log(resp.data);
+//   return resp.data;
+// }
 
-    categoryList.insertAdjacentHTML('beforeend', markup);
-  } catch (error) {
-    console.log(error);
-  }
-}
-titlesCategories();
+// function categoriesMarkup(data) {
+//   return data
+//     .map(
+//       ({ list_name }) =>
+//         `<li>
+//             <button type="button" class="category-item" data-category="${list_name}>${list_name}</button>
+//         </li>`
+//     )
+//     .join(' ');
+// }
+
+// async function titlesCategories() {
+//   try {
+//     const data = await GetCategoryList();
+//     const markup = renderCategories(data);
+//     // console.log(markup);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+// titlesCategories();
