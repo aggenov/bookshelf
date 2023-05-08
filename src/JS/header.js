b55e9069fb731704f3ab699e5b765a0a9ca7c895
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-05-08 14:11:04 +0200

    js
    створила слухача подій та зберегла у локальному сховищі теми

--- src/JS/header.js
+++ src/JS/header.js
@@ -1,8 +1,8 @@
 
 
-const burgerMenu = document.querySelector('.modal-mobile-menu');
 const btnOpen = document.querySelector('.header-burger');
 const closeModalMenu = document.querySelector('.mobile-close-btn');
+const checkbox = document.querySelector('.switch');
 
 
 btnOpen.addEventListener("click", () => {
@@ -12,3 +12,18 @@ btnOpen.addEventListener("click", () => {
 closeModalMenu.addEventListener("click", () => {
     burgerMenu.style.display = "none";
 });
+
+checkbox.addEventListener("click", toggleCheckbox);
+
+function toggleCheckbox() {
+    localStorage.setItem("ui-theme", "light");
+const themeLight = localStorage.getItem("ui-theme");
+    console.log(themeLight);
+    if (themeLight != "light") {
+        localStorage.setItem("ui-theme", "dark");
+        const themeDark = localStorage.getItem("ui-theme");
+        console.log(themeDark);
+    }
+}
+
+
