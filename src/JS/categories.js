5d1b7d29d5d71af1d38d70b959c4009b44862584
RenTensei <scs.bon7@gmail.com>
2023-05-08 13:40:21 +0300

    fix markup, fix styles, fix async problems

--- src/JS/categories.js
+++ src/JS/categories.js
@@ -1,32 +1,37 @@
 import axios from 'axios';
-import { BASE_URL } from "./home-by-category";
+import { getData } from './BestSellers/request';
+import { BASE_URL } from './home-by-category';
 
-const categoryList = document.querySelector('.categories-list')
-console.log(categoryList)
+const categoryList = document.querySelector('.categories-list');
+
+getData('/books/category-list').then(console.log);
 
 async function GetCategoryList() {
-    const resp = await axios.get(`${BASE_URL}category-list`);
-    console.log(resp.data);
-    return resp.data;
-};
+  const resp = await axios.get(`${BASE_URL}category-list`);
+  console.log(resp.data);
+  return resp.data;
+}
 
 function renderCategories(data) {
-    return data.map(data =>
+  return data
+    .map(
+      data =>
         `<li>
             <button type="button" class="category-item" data-category="${data.list_name}>${data.list_name}</button>
-        </li>`).join('');
+        </li>`
+    )
+    .join('');
 }
 
 async function titlesCategories() {
-    try {
-        const data = await GetCategoryList();
-        const markup = renderCategories(data);
-        // console.log(markup);
+  try {
+    const data = await GetCategoryList();
+    const markup = renderCategories(data);
+    // console.log(markup);
 
-        categoryList.insertAdjacentHTML('beforeend', markup);
-    } catch {
-        console.log(error);
-    };
-};
+    categoryList.insertAdjacentHTML('beforeend', markup);
+  } catch (error) {
+    console.log(error);
+  }
+}
 titlesCategories();
-
