49e514c8b9c6269bbc781aad35967aefb8be0ebe
Borys Koreniak <koreniak.borys@gmail.com>
2023-05-08 17:07:35 +0300

    Прикрутив кнопки у caterories.js
    І прописав на них активні стани

--- src/JS/categories.js
+++ src/JS/categories.js
@@ -1,17 +1,41 @@
 import { getData } from './BestSellers/request';
+import { renderCategoryList, sectionRef } from "./home-by-category";
 
 const categoryList = document.querySelector('.categories-list');
+document.addEventListener('click', onClickCategoryName);
+
+export function onClickCategoryName(e) {
+    if (e.target.matches('.category-name[data-category]')) {
+        const categoryBook = e.target.getAttribute('data-category');
+        sectionRef.innerHTML = '';
+        renderCategoryList(categoryBook, sectionRef);
+        isDisableCategoryItem()
+        isActiveCategoryItem(categoryBook)
+    }
+}
+
+function isActiveCategoryItem(category) {
+    const categoryItem = document.querySelector(`.category-name[data-category="${category}"]`)
+    categoryItem.classList.add("category-is-active")
+}
+
+async function isDisableCategoryItem() {
+    const activeCategoryItem = document.querySelector('.category-is-active')
+    if (activeCategoryItem) {
+        activeCategoryItem.classList.remove("category-is-active")
+    }
+}
 
 // привет от андрея :)
 getData('/books/category-list').then(data => {
-  const markup = data.map(CategoryItem).join('');
-  // console.log(markup);
+    const markup = data.map(CategoryItem).join('');
+    // console.log(markup);
 
-  categoryList.insertAdjacentHTML('beforeend', markup);
+    categoryList.insertAdjacentHTML('beforeend', markup);
 });
 
 function CategoryItem({ list_name }) {
-  return `
-    <li class="category-name" data-category=${list_name}>${list_name}</li>
-  `;
+    return `
+    <li class="category-name" data-category="${list_name}">${list_name}</li>
+    `;
 }
--- src/JS/home-by-category.js
+++ src/JS/home-by-category.js
@@ -2,48 +2,48 @@ import axios from 'axios';
 import { bookMarkup } from "../JS/BestSellers/MarkupGenerators/Book";
 export const BASE_URL = 'https://books-backend.p.goit.global/books/';
 
-const sectionRef = document.querySelector('.by-category-container');
+export const sectionRef = document.querySelector('.by-category-container');
 
 document.addEventListener('click', onClickSeeMoreBtn);
 
 function onClickSeeMoreBtn(e) {
     if (e.target.matches('.see-more-btn[data-category]')) {
         const categoryBook = e.target.getAttribute('data-category');
+        console.log(categoryBook)
         sectionRef.innerHTML = '';
-        renderCategoryList(categoryBook);
+        renderCategoryList(categoryBook, sectionRef);
     };
 }
 
-async function GetBooksByCategory(category) {
+async function getBooksByCategory(category) {
     const resp = await axios.get(`${BASE_URL}category?category=${category}`);
-    // console.log(resp.data);
     return resp.data;
 };
 
 function getBooks(data, category) {
     return `
         <h3 class="best-sellers-header">${colorizeMarkup(category)}</h3>
-        <ul class="bookshelf">
+        <ul class="bookshelf" data-category="${category}">
             ${data.map(bookMarkup).join('')}
         </ul>`;
 };
 
-async function renderCategoryList(category) {
+export async function renderCategoryList(category, node) {
     try {
-        const data = await GetBooksByCategory(category);
+        const data = await getBooksByCategory(category);
         const markup = getBooks(data, category)
-        sectionRef.insertAdjacentHTML('beforeend', markup);
+        node.insertAdjacentHTML('beforeend', markup); 
     } catch (error) {
         console.log(error)
     };
 };
 
 function colorizeMarkup(textContent) {
-  const wordArr = textContent.split(' ');
-  const midpoint = Math.floor(wordArr.length / 2);
+    const wordArr = textContent.split(' ');
+    const midpoint = Math.floor(wordArr.length / 2);
 
-  const firstHalf = wordArr.slice(0, midpoint).join(' ');
-  const secondHalf = wordArr.slice(midpoint).join(' ');
+    const firstHalf = wordArr.slice(0, midpoint).join(' ');
+    const secondHalf = wordArr.slice(midpoint).join(' ');
 
-  return `${firstHalf} <span>${secondHalf}</span>`;
+    return `${firstHalf} <span>${secondHalf}</span>`;
 }
