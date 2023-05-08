0fe20da97647a01fae9f586659147091bc526573
RenTensei <scs.bon7@gmail.com>
2023-05-08 14:20:27 +0300

    delete extra styles and js code

--- src/JS/categories.js
+++ src/JS/categories.js
@@ -1,15 +1,12 @@
-import axios from 'axios';
 import { getData } from './BestSellers/request';
-import { BASE_URL } from './home-by-category';
 
 const categoryList = document.querySelector('.categories-list');
 
 // привет от андрея :)
 getData('/books/category-list').then(data => {
-  console.log(data);
   const markup = data.map(CategoryItem).join('');
-
   // console.log(markup);
+
   categoryList.insertAdjacentHTML('beforeend', markup);
 });
 
@@ -18,33 +15,3 @@ function CategoryItem({ list_name }) {
     <li class="category-name" data-category=${list_name}>${list_name}</li>
   `;
 }
-
-// все что ниже нам больше не нужно :)
-
-// async function GetCategoryList() {
-//   const resp = await axios.get(`${BASE_URL}category-list`);
-//   console.log(resp.data);
-//   return resp.data;
-// }
-
-// function categoriesMarkup(data) {
-//   return data
-//     .map(
-//       ({ list_name }) =>
-//         `<li>
-//             <button type="button" class="category-item" data-category="${list_name}>${list_name}</button>
-//         </li>`
-//     )
-//     .join(' ');
-// }
-
-// async function titlesCategories() {
-//   try {
-//     const data = await GetCategoryList();
-//     const markup = renderCategories(data);
-//     // console.log(markup);
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-// titlesCategories();
