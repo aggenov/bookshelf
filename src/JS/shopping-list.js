f8a39a483a8d95073ac4ea861c4f6bda1359f012
MariySemenenko <mashyni92@gmail.com>
2023-05-09 22:38:20 +0300

    js

---
+++ src/JS/shopping-list.js
@@ -0,0 +1,281 @@
+const savedBooks = [
+  {
+    _id: '642fd89ac8cf5ee957f122da',
+    list_name: 'Paperback Nonfiction',
+    date: '2023-04-07T08:46:57.000Z',
+    age_group: '',
+    amazon_product_url:
+      'http://www.amazon.com/The-Body-Keeps-Score-Healing/dp/0670785938?tag=NYTBSREV-20',
+    article_chapter_link: '',
+    author: 'Bessel van der Kolk',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9780670785933.jpg',
+    book_image_width: 128,
+    book_image_height: 193,
+    book_review_link:
+      'https://www.nytimes.com/2018/10/18/books/review/how-to-rewire-your-traumatized-brain.html',
+    book_uri: 'nyt://book/e3e33e9d-0e67-5fec-b0d2-2ecddc38ce0e',
+    contributor: 'by Bessel van der Kolk',
+    contributor_note: '',
+    created_date: '2023-04-05 22:05:27',
+    description:
+      'How trauma affects the body and mind, and innovative treatments for recovery.',
+    first_chapter_link: '',
+    price: '0.00',
+    primary_isbn10: '0143127748',
+    primary_isbn13: '9780143127741',
+    publisher: 'Penguin',
+    rank: 1,
+    rank_last_week: 1,
+    sunday_review_link: '',
+    title: 'THE BODY KEEPS THE SCORE',
+    updated_date: '2023-04-05 22:10:17',
+    weeks_on_list: 232,
+    buy_links: [
+      {
+        name: 'Amazon',
+        url: 'http://www.amazon.com/The-Body-Keeps-Score-Healing/dp/0670785938?tag=NYTBSREV-20',
+      },
+      {
+        name: 'Apple Books',
+        url: 'https://goto.applebooks.apple/9780143127741?at=10lIEQ',
+      },
+      {
+        name: 'Barnes and Noble',
+        url: 'https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9780143127741',
+      },
+      {
+        name: 'Books-A-Million',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FTHE%252BBODY%252BKEEPS%252BTHE%252BSCORE%252FBessel%252Bvan%252Bder%252BKolk%252F9780143127741&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DTHE%252BBODY%252BKEEPS%252BTHE%252BSCORE%252BBessel%252Bvan%252Bder%252BKolk',
+      },
+      {
+        name: 'Bookshop',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9780143127741&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DTHE%2BBODY%2BKEEPS%2BTHE%2BSCORE',
+      },
+      {
+        name: 'IndieBound',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9780143127741%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DTHE%2BBODY%2BKEEPS%2BTHE%2BSCORE%2BBessel%2Bvan%2Bder%2BKolk%26aff%3DNYT',
+      },
+    ],
+    __v: 0,
+  },
+  {
+    _id: '642fd89ac8cf5ee957f122db',
+    list_name: 'Paperback Nonfiction',
+    date: '2023-04-07T08:46:57.000Z',
+    age_group: '',
+    amazon_product_url: 'https://www.amazon.com/dp/0525657746?tag=NYTBSREV-20',
+    article_chapter_link: '',
+    author: 'Michelle Zauner',
+    book_image:
+      'https://storage.googleapis.com/du-prd/books/images/9780525657743.jpg',
+    book_image_width: 338,
+    book_image_height: 500,
+    book_review_link: '',
+    book_uri: 'nyt://book/520cb070-10ce-5d43-9b0c-e77f57d84a8d',
+    contributor: 'by Michelle Zauner',
+    contributor_note: '',
+    created_date: '2023-04-05 22:05:27',
+    description:
+      'The daughter of a Korean mother and Jewish American father, and leader of the indie rock project Japanese Breakfast, describes creating her own identity after losing her mother to cancer.',
+    first_chapter_link: '',
+    price: '0.00',
+    primary_isbn10: '1984898957',
+    primary_isbn13: '9781984898951',
+    publisher: 'Vintage',
+    rank: 2,
+    rank_last_week: 0,
+    sunday_review_link: '',
+    title: 'CRYING IN H MART',
+    updated_date: '2023-04-05 22:10:17',
+    weeks_on_list: 1,
+    buy_links: [
+      {
+        name: 'Amazon',
+        url: 'https://www.amazon.com/dp/0525657746?tag=NYTBSREV-20',
+      },
+      {
+        name: 'Apple Books',
+        url: 'https://goto.applebooks.apple/9781984898951?at=10lIEQ',
+      },
+      {
+        name: 'Barnes and Noble',
+        url: 'https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9781984898951',
+      },
+      {
+        name: 'Books-A-Million',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FCRYING%252BIN%252BH%252BMART%252FMichelle%252BZauner%252F9781984898951&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DCRYING%252BIN%252BH%252BMART%252BMichelle%252BZauner',
+      },
+      {
+        name: 'Bookshop',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9781984898951&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DCRYING%2BIN%2BH%2BMART',
+      },
+      {
+        name: 'IndieBound',
+        url: 'https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9781984898951%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DCRYING%2BIN%2BH%2BMART%2BMichelle%2BZauner%26aff%3DNYT',
+      },
+    ],
+    __v: 0,
+  },
+];
+function getBookById(id) {
+  const savedBooks = JSON.parse(localStorage.getItem('savedBooks'));
+  if (!savedBooks) {
+    return null; // no saved books found
+  }
+  const book = savedBooks.map(book => {
+    const amazonUrl = book.buy_links.find(
+      link => link.name === 'Amazon'
+    ).url;
+    const appleBooksUrl = book.buy_links.find(
+      link => link.name === 'Apple Books'
+    ).url;
+    const barnesAndNobleUrl = book.buy_links.find(
+      link => link.name === 'Barnes and Noble'
+    ).url
+  return book || null; // return book if found, or null if not found
+})
+.join('');
+} 
+
+
+
+
+
+
+//******************************************************************************* */
+// const shoppingList = document.querySelector('.shopping-main-list-js');
+// const emptyList = document.querySelector('.empty-list');
+
+
+// const URL = 'https://books-backend.p.goit.global/books/{id}';
+
+
+// Отримуємо збережені у localStorage книги користувача
+// const dataBooks = JSON.parse(localStorage.getItem('LOCAL_KEY')) || [];
+// console.log(dataBooks);
+
+
+// const renderShoppingList = dataBooks => {
+//     if (Array.isArray(dataBooks)) {
+//       const cardMarkup = createCardMarkup(dataBooks);
+//       shoppingList.insertAdjacentHTML('beforeend', cardMarkup);
+//       shoppingList.addEventListener('click', removeCardMarkup);
+//     } else {
+//       shoppingList.classList.add('visually-hidden');
+//       emptyList.classList.remove('visually-hidden');
+//     }
+//   };
+
+//   function createCardMarkup(dataBooks) {
+//     if (!dataBooks || dataBooks.length === 0) {
+//       shoppingList.classList.add('visually-hidden');
+      
+//     }
+//     if (dataBooks.length > 0) {
+//       emptyList.classList.add('visually-hidden');
+//       shoppingList.classList.remove('visually-hidden');
+
+//       return dataBooks
+//       .map(book => {
+//         const amazonUrl = book.buy_links.find(
+//           link => link.name === 'Amazon'
+//         ).url;
+//         const appleBooksUrl = book.buy_links.find(
+//           link => link.name === 'Apple Books'
+//         ).url;
+//         const barnesAndNobleUrl = book.buy_links.find(
+//           link => link.name === 'Barnes and Noble'
+//         ).url;
+  
+//           return 
+//           `<li class="shopping__item shopping__item-mobile" data-id=${ book._id }>
+
+//           <button type="button" data-action='delete' value="remove" class="shopping-button">
+
+//           <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+// <path d="M17.8333 10.8333V7.33333C17.8333 4.11167 15.2217 1.5 12 1.5C8.77834 1.5 6.16667 4.11167 6.16667 7.33333V10.8333M7.1 22.5H16.9C18.8602 22.5 19.8403 22.5 20.589 22.1185C21.2475 21.783 21.783 21.2475 22.1185 20.589C22.5 19.8403 22.5 18.8602 22.5 16.9V16.4333C22.5 14.4731 22.5 13.4931 22.1185 12.7444C21.783 12.0858 21.2475 11.5504 20.589 11.2148C19.8403 10.8333 18.8602 10.8333 16.9 10.8333H7.1C5.13982 10.8333 4.15972 10.8333 3.41103 11.2148C2.75247 11.5504 2.21703 12.0858 1.88148 12.7444C1.5 13.4931 1.5 14.4731 1.5 16.4333V16.9C1.5 18.8602 1.5 19.8403 1.88148 20.589C2.21703 21.2475 2.75247 21.783 3.41103 22.1185C4.15972 22.5 5.13982 22.5 7.1 22.5Z" stroke="#F6F6F6" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+// </svg>
+// <button/>
+
+
+// <div class='shopping-tablet'>
+// <div class='flex-box-tablet'>
+//             <img
+//               src="${book.book_image}"
+//               alt="book"
+//               class="shopping__img"
+//               loading="lazy"
+//             />
+//     <div class="flex-box__text-tablet">
+//           <div class='flex-box__information'>
+//             <h2 class="shopping__title">${book.title}</h2>
+//             <p class="shopping__titleText">${book.list_name}</p>
+//             <p class='shopping__text'>${book.description}</p>
+//           </div>
+//           <div class="shopping-box-tablet">
+//             <p class='shopping__author'>${book.author}</p>
+//             <ul class="shops-list">
+//               <li>
+//                 <a
+//                   class="shops-list__link amazon"
+//                   href="${amazonUrl}"
+//                   target="_blank"
+//                   rel="noopener noreferrer"
+//                 >
+//                 <img class='shops-list__img' srcset="
+//                 ${('../../../images/amazon.png')} 48w,
+//                 ${('../../../images/amazon@2x.png')} 96w,
+//                 "
+//                 sizes="48px" src="${('./images./amazom.png')}" width="48" height="15" />
+//                 </a>
+                
+//               </li>
+//               <li>
+//                 <a
+//                   class="shops-list__link"
+//                   href="${appleBooksUrl}"
+//                   target="_blank"
+//                   rel="noopener noreferrer"
+//                 >
+//                 <img class='shops-list__img' srcset="
+//                 ${('../../../images/books.png')} 28w,
+//                 ${('../../../images/books@2x.png')} 56w,
+//                 "
+//                 sizes="28px" src="${('./images./book-shop.png')}" width="28" height="27" />
+//                 </a>
+//               </li>
+//               <li>
+//                 <a
+//                   class="shops-list__link"
+//                   href="${barnesAndNobleUrl}"
+//                   target="_blank"
+//                   rel="noopener noreferrer"
+//                 >
+//                 <img class='shops-list__img' srcset="
+//                 ${('../../../images/book-shop.png')} 32w,
+//                 ${('../../../images/book-shop2x.png')} 64w,
+//                 "
+//                 sizes="32px" src="${('./images./books.png')}" width="32" height="30" />
+//                 </a>
+//               </li>
+//             </ul>
+//           </div>
+//     </div> 
+// </div>
+// </div>
+// </li>`;
+// })
+// .join('');
+// } 
+// }
+
+          
+
+//   async function removeCardMarkup(event) {
+//     if (event.target.dataset.action !== 'delete') {
+//       return;
+//     }
+// }
+
+
