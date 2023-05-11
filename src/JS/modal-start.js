833b857ffee89a0a9128374761fe9f67274bb155
aggenov <a.g.genov@gmail.com>
2023-05-11 11:37:03 +0300

    add

--- src/JS/modal-start.js
+++ src/JS/modal-start.js
@@ -1,9 +1,18 @@
-const form = document.querySelector('.form-start');
-
 const btnOpenStart = document.querySelector('.btn-signup');
 const backdropStart = document.querySelector('.backdrop-start');
 const btnCloseStart = document.querySelector('.btn-close-modal');
+
 btnOpenStart.addEventListener('click', openModalStart);
+backdropStart.addEventListener('click', event => {
+  if (event.target.matches('.backdrop-start')) {
+    closeModalStart();
+  }
+});
+document.addEventListener('keydown', evt => {
+  if (evt.key === 'Escape') {
+    closeModalStart();
+  }
+});
 
 function openModalStart() {
   backdropStart.classList.remove('is-hidden');
@@ -14,56 +23,69 @@ function closeModalStart() {
   btnCloseStart.removeEventListener('click', closeModalStart);
 }
 
+const form = document.querySelector('.form-start');
+
 const USER_KEY = 'USER_KEY';
 const { name, email, password, button } = form;
 const starData = localStorage.getItem(USER_KEY);
 const parsData = JSON.parse(starData);
 //______________________________________________
+const USER_DATA = {
+  name: Alex,
+  email: 'user@mail.com',
+  password: 'secret',
+};
+//______________________________________________
+if (parsData) {
+  form.elements.name.value = parsData.userName;
+  form.elements.email.value = parsData.userEmail;
+  form.elements.password.value = parsData.userPassword;
+  form.elements.name.disabled = true;
+  form.elements.email.disabled = true;
+  form.elements.password.disabled = true;
+  // form.elements.button.textContent = 'logout';
+}
 
-// if (parsData) {
-//   form.elements.email.value = parsData.userEmail;
-//   form.elements.password.value = parsData.userPassword;
-//   form.elements.email.disabled = true;
-//   form.elements.password.disabled = true;
-//   form.elements.button.textContent = 'logout';
-// }
+form.addEventListener('submit', onSubmit);
 
-// form.addEventListener('submit', onSubmit);
+function onSubmit(evt) {
+  evt.preventDefault();
+  if (JSON.parse(localStorage.getItem(USER_KEY))) {
+    form.elements.name.disabled = false;
+    form.elements.email.disabled = false;
+    form.elements.password.disabled = false;
+    form.elements.button.textContent = 'login';
 
-// function onSubmit(evt) {
-//   evt.preventDefault();
-//   if (JSON.parse(localStorage.getItem(LOCAL_KEY))) {
-//     form.elements.email.disabled = false;
-//     form.elements.password.disabled = false;
-//     form.elements.button.textContent = 'login';
-
-//     localStorage.removeItem(LOCAL_KEY);
-//     form.reset();
-//     return;
-//   }
-//   if (
-//     form.elements.email.value !== USER_DATA.email ||
-//     form.elements.password.value !== USER_DATA.password
-//   ) {
-//     alert('Введіть правільні данні');
-//     return;
-//   }
-//   const formInput = {
-//     userEmail: form.elements.email.value,
-//     userPassword: form.elements.password.value,
-//   };
-//   localStorage.setItem(LOCAL_KEY, JSON.stringify(formInput));
-//   form.elements.email.disabled = true;
-//   form.elements.password.disabled = true;
-//   form.elements.button.textContent = 'logout';
-// }
+    localStorage.removeItem(USER_KEY);
+    form.reset();
+    return;
+  }
+  if (
+    form.elements.name.value !== USER_DATA.name ||
+    form.elements.email.value !== USER_DATA.email ||
+    form.elements.password.value !== USER_DATA.password
+  ) {
+    alert('Введіть правільні данні');
+    return;
+  }
+  const formInput = {
+    userName: form.elements.name.value,
+    userEmail: form.elements.email.value,
+    userPassword: form.elements.password.value,
+  };
+  localStorage.setItem(USER_KEY, JSON.stringify(formInput));
+  form.elements.name.disabled = true;
+  form.elements.email.disabled = true;
+  form.elements.password.disabled = true;
+  form.elements.button.textContent = 'logout';
+}
 
 //______________________________________________
 // const formInput = document.querySelector('.form-imput');
 
-form.addEventListener('submit', onclickInput);
+// form.addEventListener('submit', onclickInput);
 
-function onclickInput(evt) {
-  evt.preventdefault();
-  console.dir(form);
-}
+// function onclickInput(evt) {
+//   evt.preventdefault();
+//   console.dir(form);
+// }
