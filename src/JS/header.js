80ff733d8699287bd9a3e62a412346dbe44398c5
aggenov <a.g.genov@gmail.com>
2023-05-11 13:23:27 +0300

    fix svg

--- src/JS/header.js
+++ src/JS/header.js
@@ -34,3 +34,21 @@ function toggleCheckbox() {
     localStorage.setItem('ui-theme', 'dark');
   }
 }
+
+function myFunction() {
+  document.getElementById('myDropdown').classList.toggle('show');
+}
+
+// Закройте выпадающее меню, если пользователь щелкает за его пределами
+window.onclick = function (event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName('dropdown-content');
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+};
