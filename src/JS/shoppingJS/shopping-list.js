358f806e34980396d15112cd56059115bb5f16a2
RenTensei <scs.bon7@gmail.com>
2023-05-11 11:38:07 +0300

    emtylist improvements

--- src/JS/shoppingJS/shopping-list.js
+++ src/JS/shoppingJS/shopping-list.js
@@ -5,18 +5,17 @@ const shoppingList = document.querySelector('.shopping-cart');
 const emptyList = document.querySelector('.empty-list');
 
 function renderShoppingList(BooksArray) {
+  if (loadStorageBooks().length == 0) {
+    emptyList.classList.remove('visually-hidden');
+    return;
+  }
+
   if (Array.isArray(BooksArray)) {
     // проходимся мапом по каждому объекту из массива
     const cardMarkup = BooksArray.map(createBookMarkup).join(' ');
 
     shoppingList.insertAdjacentHTML('beforeend', cardMarkup);
   }
-
-  // не люблю элсы
-  // else {
-  //   shoppingList.classList.add('visually-hidden');
-  //   emptyList.classList.remove('visually-hidden');
-  // }
 }
 
 // вызов функции loadStorageBooks() вернет массив книг из локалсторедж.
