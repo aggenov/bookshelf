4febf41d70300c6345258d28ceb65156b187fc96
Borys Koreniak <koreniak.borys@gmail.com>
2023-05-12 18:18:49 +0300

    Прибрав зайві коментарі

--- src/JS/home-by-category.js
+++ src/JS/home-by-category.js
@@ -6,20 +6,6 @@ export const BASE_URL = 'https://books-backend.p.goit.global/books/';
 
 export const sectionRef = document.querySelector('.by-category-container');
 
-// Додав слухача по кліку на книгу
-// document.addEventListener('click', onClickBookItem);
-// Функція, яка записує у localStorage клацнуту книгу
-// function onClickBookItem(e) {
-// 	if (e.target.matches('.book-item[data-modal-id]')) {
-// 		const idBook = e.target.getAttribute('data-modal-id');
-
-// 		getData(`/books/${idBook}`).then(data => {
-// 			const bookById = data;
-// 			localStorage.setItem('data-book-by-id', JSON.stringify(bookById));
-// 		});
-// 	};
-// };
-
 document.addEventListener('click', onClickSeeMoreBtn);
 
 function onClickSeeMoreBtn(e) {
